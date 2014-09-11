c29e0326ea103629e1ff3efa196c5a205c780f12
Lindsey-Eidson <lindseidson@gmail.com>
2014-09-11 15:48:11 -0400

    Original basis for the Player Class

---
+++ Player.java
@@ -0,0 +1,56 @@
+public class Player {
+    private String name;
+    private int diff;
+    private int pilotPoints;
+    private int fighterPoints;
+    private int traderPoints;
+    private int engineerPoints;
+
+    private static int DEFAULT_DIFF = 1;
+    private static int DEFAULT_SKILL = 4;
+
+    public Player(String name, int diff, int pilotPoints, fighterPoints,
+        int traderPoints, int engineerPoints) {
+            this.name = name;
+            this.diff = diff;
+            this.pilotPoints = pilotPoints;
+            this.fighterPoints = fighterPoints;
+            this.traderPoints = traderPoints;
+            this.engineerPoints = engineerPoints;
+    }
+
+
+    public Player(String name) {
+        this(name, DEFAULT_DIFF) 
+    }
+
+    public Player(String name, int diff) {
+        this (name, diff, DEFAULT_SKILL, DEFAULT_SKILL, DEFAULT_SKILL,
+            DEFAULT_SKILL)
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public int diff() {
+        return diff;
+    }
+
+    public int pilot() {
+        return pilotPoints;
+    }
+
+    public int fighter() {
+        return fighterPoints;
+    }
+
+    public int trader() {
+        return traderPoints;
+    }
+
+    public int engineer() {
+        return engineerPoints;
+    }
+
+}
