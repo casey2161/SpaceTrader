aafeb551222c76a101d1e177644aca5848315042
Nickolas Graham <ngraham7@gatech.edu>
2014-09-16 15:23:08 -0400

    Moved ship and weapon

---
+++ SpaceTrader/src/spacetrader/Universe/Weapon.java
@@ -0,0 +1,11 @@
+package SpaceTrader;
+
+public abstract class Weapon {
+	protected int damage, accuracy, ammo;
+	
+	public Weapon (int damage, int accuracy, int ammo) {
+		this.damage = damage;
+		this.accuracy = accuracy;
+		this.ammo = ammo;
+	}
+}
