8686cce0e26d6d7576895bd07023d8ca2647ed9c
gli74 <gli94@gatech.edu>
2014-09-18 13:44:40 -0400

    Updates to Ship and Weapon

--- SpaceTrader/src/spacetrader/Universe/Ship.java
+++ SpaceTrader/src/spacetrader/Universe/Ship.java
@@ -1,10 +1,47 @@
-public abstract class Ship {
-	protected int shield, speed;
-	protected Weapon weapon;
+public class Ship {
+	private String name;
+	private int maxRange, size, quality, shield, currRange;
+	private Weapon weapon;
+	private Equipment[] equipmentSlots;
 	
-	public Ship (int shield, int speed, Weapon weapon) {
-		this.shield = shield;
-		this.speed = speed;
-		this.weapon = weapon;
+	public Ship (String name, int maxRange, int size, int quality) {
+		this.name = name;
+		this.maxRange = maxRange;
+		this.quality = quality;
+		this.size = size;
+		shield = size*quality;
+		currRange = maxRange;
+	}
+	
+	public void addWeapon(Weapon newWeapon) {
+		if (quality >= newWeapon.getMinQuality()) {
+			weapon = newWeapon;
+		} else {
+			System.out.println("Your ship is too low-tech to use this weapon!");
+		}
+	}
+	
+	public void travel(int distance) {
+		if (distance > currRange) {
+			System.out.println("Planet is outside of the current range of your ship!");
+		} else {
+			currRange = currRange - distance;
+		}
+	}
+	
+	public void refuel() {
+		currRange = maxRange;
+	}
+	
+	public void takeDamage(int damageTaken) {
+		if (shield - damageTaken <= 0) {
+			System.out.println("Your ship has been destroyed!");
+		} else {
+			shield = shield - damageTaken;
+		}
+	}
+	
+	public void dealDamage (Ship opponent, int damageDealt) {
+		opponent.takeDamage(damageDealt);
 	}
 }
--- SpaceTrader/src/spacetrader/Universe/Weapon.java
+++ SpaceTrader/src/spacetrader/Universe/Weapon.java
@@ -1,9 +1,26 @@
 public abstract class Weapon {
-	protected int damage, accuracy, ammo;
+	protected int damage, accuracy, ammo, minQuality;
 	
-	public Weapon (int damage, int accuracy, int ammo) {
+	public Weapon (int damage, int accuracy, int ammo, int minQuality) {
 		this.damage = damage;
 		this.accuracy = accuracy;
 		this.ammo = ammo;
+		this.minQuality = minQuality;
+	}
+	
+	public int getDamage() {
+		return damage;
+	}
+	
+	public int getAccuracy() {
+		return accuracy;
+	}
+	
+	public int getAmmo() {
+		return ammo;
+	}
+	
+	public int getMinQuality() {
+		return minQuality;
 	}
 }
