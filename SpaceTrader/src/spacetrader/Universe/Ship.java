f6ee9f471b50570bb6e83867dd15567d227c7872
gli74 <gli94@gatech.edu>
2014-09-18 15:17:02 -0400

    Updated travel method for Ship

--- SpaceTrader/src/spacetrader/Universe/Ship.java
+++ SpaceTrader/src/spacetrader/Universe/Ship.java
@@ -1,14 +1,18 @@
+import java.lang.Math;
+
 public class Ship {
 	private String name;
 	private int maxRange, size, quality, shield, currRange;
 	private Weapon weapon;
+	private Planet location;
 	private Equipment[] equipmentSlots;
 	
-	public Ship (String name, int maxRange, int size, int quality) {
+	public Ship (String name, int maxRange, int size, int quality, Planet location) {
 		this.name = name;
 		this.maxRange = maxRange;
 		this.quality = quality;
 		this.size = size;
+		this.location = location;
 		shield = size*quality;
 		currRange = maxRange;
 	}
@@ -21,11 +25,14 @@ public class Ship {
 		}
 	}
 	
-	public void travel(int distance) {
+	public void travel(Planet destination) {
+		int distance = Math.sqrt(Math.pow(destination.getX() - location.getX(), 2) + Math.pow(
+				destination.getY() - location.getY(), 2));
 		if (distance > currRange) {
 			System.out.println("Planet is outside of the current range of your ship!");
 		} else {
 			currRange = currRange - distance;
+			location = destination;
 		}
 	}
 	
