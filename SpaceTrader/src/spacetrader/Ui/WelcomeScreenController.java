d60c1f977e6d3b8f5dc02254d648935bd05d8ebb
Nickolas Graham <ngraham7@gatech.edu>
2014-10-20 21:13:08 -0400

    UI Rough Drafts
    Rough Ui changes here and there. Still need to figure out color schemes
    and general design language but this is after playing around

--- SpaceTrader/src/spacetrader/Ui/WelcomeScreenController.java
+++ SpaceTrader/src/spacetrader/Ui/WelcomeScreenController.java
@@ -6,53 +6,70 @@
 
 package spacetrader.Ui;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.ObjectInputStream;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.stage.FileChooser;
-import spacetrader.Player;
-import spacetrader.SpaceTrader;
-import spacetrader.Universe.Universe;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.scene.effect.Reflection;
 
 /**
  *
  * @author andikaputra
  */
 public class WelcomeScreenController implements Initializable {
+    private static Stage stage;
+    private static Scene[] allScenes;
+    @FXML private ImageView bgImage;
+    @FXML private Button fontButton;
+    @FXML private Button fontButton2;
+    @FXML private Button fontButton3;
     
     @FXML
     private void handleNewGameButtonAction(ActionEvent event) {
-        SpaceTrader.stage.setScene(SpaceTrader.allScenes[1]);
+        stage.setScene(allScenes[1]);
     }
     
     @FXML
     private void handleLoadGameButtonAction(ActionEvent event) {
-        try {
-            FileChooser fileChooser = new FileChooser();
-            File file = fileChooser.showOpenDialog(SpaceTrader.stage);
-            FileInputStream fis = new FileInputStream(file);
-            ObjectInputStream ois = new ObjectInputStream(fis);
-            Universe.setInstance((Universe) ois.readObject());
-            Player.setInstance((Player) ois.readObject());
-            SpaceTrader.stage.setScene(SpaceTrader.allScenes[2]);
-        } catch(Exception e) {
-            //Shouldn't happen
-            e.printStackTrace();
-        }
+        System.out.println("Load Game feature has not been implemented yet."); // Replace later.
     }
     
     @FXML
     private void quitAction(ActionEvent event) {
         System.exit(0);
     }
-
+    
+    @FXML
+    private void handleMouseMoved(MouseEvent event) {
+        double x = event.getX();
+        double y = event.getY();
+        double imageX = bgImage.getX();
+        double imageY = bgImage.getY();
+        if (x <= 180 || x >= -180) {
+            bgImage.setX(-x + (x/5));
+        } 
+        if (y <= 200 || y >= -100) {
+            bgImage.setY(-y + (y/5));
+        }
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        Reflection reflection = new Reflection();
+        fontButton.setEffect(reflection);
+        fontButton2.setEffect(reflection);
+        fontButton3.setEffect(reflection);
     }
+
+    public static void passStageAndScene(Stage mainStage, Scene[] scenes) {
+        stage = mainStage;
+        allScenes = scenes;
+    }
+    
 }
