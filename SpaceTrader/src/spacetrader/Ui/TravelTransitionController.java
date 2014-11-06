7bb152454d9a15b6ebeb05681822147bc42ed9a5
Lindsey-Eidson <lindseidson@gmail.com>
2014-11-06 15:28:48 -0500

    TravelTransition

---
+++ SpaceTrader/src/spacetrader/Ui/TravelTransitionController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spacetrader.Ui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Gateway
+ */
+public class TravelTransitionController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
