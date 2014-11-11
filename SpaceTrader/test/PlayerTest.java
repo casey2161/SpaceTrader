98599b9ebeb85ee8b361acf0995d7d9504fe0587
Lindsey-Eidson <lindseidson@gmail.com>
2014-11-11 15:19:35 -0500

    Fixed Travel() Junit

--- SpaceTrader/test/PlayerTest.java
+++ SpaceTrader/test/PlayerTest.java
@@ -14,14 +14,14 @@ public class PlayerTest {
         Player.clearInstance();
         Universe.recreateInstance();
         Planet start = new Planet("Start", 1, 1, 0, 0, false, null, null);
-        Player p1 = new Player("Lindsey", 1, 5, 5, 5, 5, start);
-        System.out.println(p1.location().getX() + "," + p1.location().getY());
+        Player.updateInstance("Lindsey", 1, 5, 5, 5, 5, start);
+        System.out.println(Player.getInstance().location().getX() + "," + Player.getInstance().location().getY());
         Planet destination = new Planet("TestPlanet", 1, 1, 28, 70, false, null, null);
-        assertTrue("Check your code", p1.canTravel(destination));
-        p1.travel(destination);
+        assertTrue("Check your code", Player.getInstance().canTravel(destination));
+        Player.getInstance().travel(destination);
         Planet newDest = new Planet("New Dest", 1, 1, 300, 500, false, null, null);
-        assertFalse("Check your code", p1.canTravel(newDest));
-        assertEquals(destination, p1.location());
+        assertFalse("Check your code", Player.getInstance().canTravel(newDest));
+        assertEquals(destination, Player.getInstance().location());
 
 
     }
